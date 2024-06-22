6c4f0c88882624dadaf24235fe4418128a9b513c
l-rollon <100705417+l-rollon@users.noreply.github.com>
2024-06-22 11:23:59 +0200

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std; 
+
+int main()
+{
+	int a, b; 
+	cout << "1- VAMOS A JUGAR\n";
+	cout << "2- CONTROLES\n";
+	cout << "3- Salir\n";
+	cout << "La opcion deseada es: ";
+	cin >> a; 
+	do {
+		switch (a)
+		{
+		case 1:
+		case 2:
+		case 3:
+			cout << "¿Seguro que desea salir?  SI o NO\n";
+			cout << "1- SI\n";
+			cout << "2- NO\n";
+			cout << "La opcion elegida es: ";
+			cin >> b;
+			// REGRESO AL MENU PARA ELEGIR O SALIR DEL PROGRAMA
+			if (b == 1)
+			{
+				return 0;
+			}
+			else
+			{
+				return main();
+			}
+			break;
+		}
+	} while (a != 3); //mientras sea distinto de 4 sigue 
+
+	return 0;
+}
