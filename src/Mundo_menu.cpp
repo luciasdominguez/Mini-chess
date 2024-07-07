1ad5d0ac184b31e9bb11c3947cedaf3c1ea1f736
Lucía <lucia.sdominguez@alumnos.upm.es>
2024-07-07 02:29:35 +0200

    Mejora de legibilidad del menú

--- src/Mundo_menu.cpp
+++ src/Mundo_menu.cpp
@@ -24,19 +24,19 @@ void Mundo_menu::dibuja_menu()
 	caja.dibuja();
 
 	ETSIDI::setTextColor(1, 1, 1);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 16);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 16);
 	ETSIDI::printxy("AJEDREZ GRAVITATORIO", -10, 36);
 	ETSIDI::printxy("MENU DE OPCIONES", -8, 33.5);
 
 	ETSIDI::setTextColor(1, 1, 1);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 15);
 	ETSIDI::printxy("INFORMATICA INDUSTRIAL", 3, -5);
 
 	ETSIDI::setTextColor(0, 0, 0);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 20);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 15);
 	ETSIDI::printxy("1- VAMOS A JUGAR", -10, 20);
-	ETSIDI::printxy("2- COMO JUGAR", -10, 16);
-	ETSIDI::printxy("3- SALIR", -10, 12);
+	ETSIDI::printxy("2- COMO JUGAR", -9, 16);
+	ETSIDI::printxy("3- SALIR", -6, 12);
 }
 
 void Mundo_menu::dibuja_controles()
@@ -45,27 +45,25 @@ void Mundo_menu::dibuja_controles()
 		0.0, y_ojo, 0.0,
 		0.0, 1.0, 0.0);
 	ETSIDI::setTextColor(1, 1, 1);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 14);
-	ETSIDI::printxy("ESTOS SON LOS CONTROLES", -12, 17);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 14);
+	ETSIDI::printxy("CONTROLES:", -12, 29);
 	ETSIDI::setTextColor(100, 0, 0);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
-	ETSIDI::printxy("PARA VOLVER AL MENU PULSE m", -6, -2);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 14);
+	ETSIDI::printxy("PARA VOLVER AL MENU PULSE m", -2, 0);
 	//escritura de los controles
 	ETSIDI::setTextColor(50, 50, 0);
-	ETSIDI::setFont("fuentes/Bitwise.ttf", 12);
-	ETSIDI::printxy("S: cargar partida O guardar y salir", -14, 16);
-	ETSIDI::printxy("N: genera partida con posiciones iniciales", -14, 15);
-	ETSIDI::printxy("E: cargar partida ejemplo", -14, 14);
-	ETSIDI::printxy("J: modo juego", -14, 13);
-	ETSIDI::printxy("P: seleccionar partida", -14, 12);
-	ETSIDI::printxy("(guion): retroceder en la jugada", -14, 11);
-	ETSIDI::printxy("+: avanzar en la jugada", -14, 10);
-	ETSIDI::printxy("5: activar cursor", -14, 9);
-	ETSIDI::printxy("(punto): selecciona pieza", -14, 8);
-	ETSIDI::printxy("0: ejecutar movimiento", -14, 7);
-	ETSIDI::printxy("(asterisco): juega la gravedad", -14, 6);
-	ETSIDI::printxy("(espacio): poner piezas en el almacen", -14, 5);
-	ETSIDI::printxy("8, 9, 6, 3, 2, 1, 4, 7: mover las piezas", -14, 4);
+	ETSIDI::setFont("fuentes/SwanseaBold.ttf", 14);
+	ETSIDI::printxy("S: cargar partida O guardar y salir", -16, 26);
+	ETSIDI::printxy("N: genera partida con posiciones iniciales", -16, 24);
+	ETSIDI::printxy("J: modo jugar", -16, 22);
+	ETSIDI::printxy("P: modo seleccionar partida", -16, 20);
+	ETSIDI::printxy("(guion): retroceder en la jugada", -16, 18);
+	ETSIDI::printxy("+: avanzar en la jugada", -16, 16);
+	ETSIDI::printxy("5: activar/desactivar cursor", -16, 14);
+	ETSIDI::printxy("(punto): seleccionar pieza", -16, 12);
+	ETSIDI::printxy("0: confirmar movimiento", -16, 10);
+	ETSIDI::printxy("(asterisco): juega la gravedad", -16, 8);
+	ETSIDI::printxy("1, 2, 3, 4, 6, 7, 8, 9: mover las piezas (según keypad)", -16, 6);
 
 }
 
