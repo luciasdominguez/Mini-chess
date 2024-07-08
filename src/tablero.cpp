dbd908b1b546264074f0d04ac589396f110bc5c2
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2024-07-08 19:12:53 +0200

    Update tablero.cpp

--- src/tablero.cpp
+++ src/tablero.cpp
@@ -18,47 +18,17 @@ tablero::tablero( int filas_, int columnas_){
 
 }
 
-//tablero::tablero(const int filas_, const int columnas_)// : n_filas(filas_), n_columnas(columnas_)
-//{
-//    n_filas = filas_;
-//    n_columnas = columnas_;
-//
-//    // crear el tablero
-//    matriz = new casilla * [filas_];
-//    for (int i = 0; i < filas_; i++) {
-//        matriz[i] = new casilla[columnas_];
-//        for (int j = 0; j < columnas_; j++) {
-//            matriz[i][j]=casilla(i,j);
-//        }
-//    }
-//}
+
 
 tablero::~tablero() {
     // Liberar la memoria del tablero
-   /* int t;
-    for (int i = 0; i < n_filas; ++i)
-    {
-        delete[] matriz[i];
-    }
-    delete[] matriz;*/
+   
 }
 
 
 
 void tablero::gravedad() {
-    //for (int i = 0; i < n_columnas; i++) {
-    //    for (int j = 1; j < n_filas; j++) {
-    //        int resta = j - 1;
-    //        while (this->tab.at(i).at(resta).ocupacion->leer_tipo() == t_NO && resta > -1) {
-    //            resta--;
-    //        }
-    //        if (this->tab.at(i).at(resta+1).ocupacion->leer_tipo() == t_NO) {
-    //            this->tab.at(i).at(resta+1).ocupacion = this->tab.at(i).at(j).ocupacion;
-    //            //Vacia v = Vacia();
-    //            this->tab.at(i).at(j).ocupacion = &v;
-    //        }
-    //    }
-    //}
+   
 
 }
 
