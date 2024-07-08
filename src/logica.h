7bda260a3d07f95ef904960250047e3691e5a42d
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2024-07-08 19:22:05 +0200

    Update logica.h

--- src/logica.h
+++ src/logica.h
@@ -5,7 +5,6 @@
 #include "fichas.h"
 #include "gestor_jaques.h"
 
-//enum ENUM_ANALISIS {  = 0, NEGRAS, GRAVEDAD_N, GRAVEDAD_B };
 enum ENUM_TRAYECTO { TR_torre = 0, TR_alfil = 1, TR_caballo = 2, TR_otro = -1 };
 
 class Cl_logica
@@ -35,15 +34,12 @@ class Cl_logica
 	//rey
 	ficha* rey_ = new Rey;
 
-	////vacia
-	//ficha* vacio = new Vacia;
-
 	//T_tablero
 	vector<vector<PIEZA_STRU>> TableroGUI_actual;
 	PIEZA_STRU busca_posicion_anterior(PIEZA_STRU pieza_jugada);
 
 
-	//void analiza_gravedad();  // por hacer...............
+	//void analiza_gravedad();  
 
 	void genera_tablero_temporal();
 
@@ -52,16 +48,16 @@ public:
 
 	void analiza_gravedad(vector<vector<PIEZA_STRU>> Tablero, GUI_jugada& jugada_propia, GUI_jugada& jugada_gravedad);
 	// Si la jugada es correcta:
-	//	se genera la "jugada_propia" pero aÒadiendo, si es el caso,
+	//	se genera la "jugada_propia" pero a√±adiendo, si es el caso,
 	//	el resto de piezas afectadas (por haber comido una pieza contraria, un enroque ...
 	//	Tambien se devuelve en "jugada_gravedad" la lista de las piezas que se ven afectadas por la gravedad.
 	// 
 	// Si la "jugaga_gravedad" ha generado un jaque mate devuelve 2 para que se termine la partida
 	// si no es jaque mate devuelve 1
-	// Si la ultima jugada en valida al funciÛn devuelve 0;
-	int analiza_jugada(vector<vector<PIEZA_STRU>> Tablero_actual_, // situaciÛn del tablero antes de realizar la "jugada_propia"
-		GUI_jugada& jugada_propia, // jugada que se debe analizar en funciÛn de la situaciÛn del tablero
-		// se devolver· la jugada actualizada si la logica del juego afecta a otra pieza
+	// Si la ultima jugada en valida al funci√≥n devuelve 0;
+	int analiza_jugada(vector<vector<PIEZA_STRU>> Tablero_actual_, // situaci√≥n del tablero antes de realizar la "jugada_propia"
+		GUI_jugada& jugada_propia, // jugada que se debe analizar en funci√≥n de la situaci√≥n del tablero
+		// se devolver√° la jugada actualizada si la logica del juego afecta a otra pieza
 		GUI_jugada& jugada_gravedad, // jugada que realiza la gravedad
 		bool jugada_erronea);  // solamete para test
 
